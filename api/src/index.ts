f12bfcad52ad20be53605e3094f12bd6f0975942
Carlos Augusto Borges <38558653+Carlosaugustob@users.noreply.github.com>
2025-06-24 22:58:20 -0300

    criação do servidor

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,35 +1,12 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/mysql2';
-import { eq } from 'drizzle-orm';
-import { instructorTable } from './db/schema';
+import  express  from 'express';
   
-const db = drizzle(process.env.DATABASE_URL!);
-async function main() {
-  const user: typeof instructorTable.$inferInsert = {
-    id? 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
-  await db.insert(instructorTable).values(user);
-  console.log('New user created!')
-  const users = await db.select().from(instructorTable);
-  console.log('Getting all users from the database: ', users)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  await db
-    .update(instructorTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(instructorTable.email, user.email));
-  console.log('User info updated!')
-  await db.delete(instructorTable).where(eq(instructorTable.email, user.email));
-  console.log('User deleted!')
+function main() {
+ 
+  const app = express();
+
+
+  app.listen(3333, () => console.log('servidor está rodando'))
+
 }
 main();
